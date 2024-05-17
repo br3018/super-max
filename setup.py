6b1c1c53de5b5478c15b77c5553f3bbfadd7d393
Ben Rose <brbenrose@gmail.com>
2024-05-17 18:46:07 +0100

    Initial codebase setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='super-max',
+    version='0.1.0',
+    description='Repository for code to calcualte best F1 fantasy team based on historical data.',
+    author='Benedict Rose',
+    author_email='brbenrose@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+)
---
+++ super_max/__init__.py
@@ -0,0 +1,17 @@
+"""
+Code to run on Raspberry Pi for avionics applications 
+"""
+
+__author__ = "Benedict Rose; Zoe Cheah"
+__version__ = "0.1.0"
+__license__ = "GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007"
+
+
+def main():
+    """
+    Main script
+    """
+
+
+if __name__ == "__main__":
+    main()
