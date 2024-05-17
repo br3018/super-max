861cc9253c725611014cc3824f0f54c7f33def25
Ben Rose <brbenrose@gmail.com>
2024-05-17 19:31:09 +0100

    Trial data entry

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # super-max
 Cheat code for finding optimal F1 fantasy teams
+
+Method works based off extracting past performances for each F1 driver and calculating expected value of points scored per race. All permutations of F1 drivers and constructors are formulated and expected score for the selection is calculated. Option to then discard permutations which exceed the allowable budget. 
--- super_max/__init__.py
+++ super_max/__init__.py
@@ -6,11 +6,22 @@ __author__ = "Benedict Rose; Zoe Cheah"
 __version__ = "0.1.0"
 __license__ = "GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007"
 
+import pandas as pd
+import numpy as np 
+import os
 
 def main():
     """
     Main script
     """
+    # Get current working directory
+    cwd = os.getcwd()
+
+    # Load data
+    data_path = os.path.join(cwd, "race_info.csv")
+    print(data_path)
+    df = pd.read_csv(data_path)
+    df.info()
 
 
 if __name__ == "__main__":
