09a8c5c1e4a8d949cb30c9dee7cec1ae9cf14042
Ben Rose <brbenrose@gmail.com>
2024-05-18 07:35:10 +0100

    Completed

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # super-max
 Cheat code for finding optimal F1 fantasy teams
 
-Method works based off extracting past performances for each F1 driver and calculating expected value of points scored per race. All permutations of F1 drivers and constructors are formulated and expected score for the selection is calculated. Option to then discard permutations which exceed the allowable budget. 
+Method works based off extracting past performances for each F1 driver and calculating expected value of points scored per race. All combinations of F1 drivers and constructors are formulated and expected score for the selection is calculated. Option to then discard combinations which exceed the allowable budget. 
+
+To run, enter most recent race data to constructor_info.csv and driver_info.csv from:
+https://fantasy.formula1.com/en/statistics/details?tab=driver&filter=fPoints
+Set COST_CAP according to your current cost cap and RACES according to how many races have happened so far. 
+
+Improvements that could be made include incorporating the DRS feature and calculating expected points by number of races each driver has done rather than total. 
--- super_max/__init__.py
+++ super_max/__init__.py
@@ -23,7 +23,10 @@ def main():
     # Number of constructors to select
     NUM_CONSTRUCTORS = 2
     # Cost cap
-    COST_CAP = 100
+    COST_CAP = 109.7
+
+    # Get working directory
+    cwd = os.getcwd()
     
     # Data url
     driver_data_url = "https://raw.githubusercontent.com/br3018/super-max/main/driver_info.csv"
@@ -31,46 +34,71 @@ def main():
 
     # Load data
     driver_df = pd.read_csv(driver_data_url)
-    driver_df.info()
-    print(driver_df.columns)
     constructor_df = pd.read_csv(constructor_data_url)
-    constructor_df.info()
-    print(constructor_df.columns)
 
     # Calculate expected points for each driver from historical data
     driver_df['expected_points'] = driver_df['points'].divide(RACES)
     # Calculate expected points for each constructor from historical data
     constructor_df['expected_points'] = constructor_df['points'].divide(RACES)
 
+    # Show dataframes
+    print(driver_df.to_string())
+    print()
+    print(constructor_df.to_string())
+    print()
+
     # Generate combinations matrix for all drivers
+    print("Calculating driver combinations (1/3)")
     driver_combinations = list(itertools.combinations(driver_df['drivers'], 5))
+    print("Found {} driver combinations".format(len(driver_combinations)))
     # Calculate expected score and cost for each team
     driver_combination_escore = np.zeros(len(driver_combinations))
     driver_combination_cost = np.zeros(len(driver_combinations))
     for i, driver_combination in enumerate(driver_combinations):
         driver_combination_escore[i] = driver_df[driver_df['drivers'].isin(driver_combination)]['expected_points'].sum()
         driver_combination_cost[i] = driver_df[driver_df['drivers'].isin(driver_combination)]['current_cost'].sum()
+    print("Calculations complete")
+    print()
     
     # Generate combinations matrix for all constructors
+    print("Calculating constructor combinations (2/3)")
     constructor_combinations = list(itertools.combinations(constructor_df['constructors'], 2))
+    print("Found {} constructor combinations".format(len(constructor_combinations)))
     # Calculate expected score and cost for each team
     constructor_combination_escore = np.zeros(len(constructor_combinations))
     constructor_combination_cost = np.zeros(len(constructor_combinations))
     for i, constructor_combination in enumerate(constructor_combinations):
         constructor_combination_escore[i] = constructor_df[constructor_df['constructors'].isin(constructor_combination)]['expected_points'].sum()
         constructor_combination_cost[i] = constructor_df[constructor_df['constructors'].isin(constructor_combination)]['current_cost'].sum()
+    print("Calculations complete")
+    print()
+
 
     # Combine driver and constructor combinations
-    team_df = pd.DataFrame(columns=['driver_combination', 'constructor_combination', 'expected_score', 'cost'])
+    print("Calculating team combinations (3/3)")
+    print("Found {} team combinations".format(len(driver_combinations) * len(constructor_combinations)))
+    team_drivers = []
+    team_constructors = []
+    team_escore = []
+    team_cost = []
     for i, constructor_combination in enumerate(constructor_combinations):
+        print("{} Percent Complete".format(i/len(constructor_combinations) * 100))
         for j, driver_combination in enumerate(driver_combinations):
-            team_df = team_df.append({'driver_combination': driver_combination, 'constructor_combination': constructor_combination, 'expected_score': driver_combination_escore[j] + constructor_combination_escore[i], 'cost': driver_combination_cost[j] + constructor_combination_cost[i]}, ignore_index=True)
+            team_drivers.append(driver_combination)
+            team_constructors.append(constructor_combination)
+            team_escore.append(driver_combination_escore[j] + constructor_combination_escore[i])
+            team_cost.append(driver_combination_cost[j] + constructor_combination_cost[i])
+    team_df = pd.DataFrame({'driver_combination': team_drivers, 'constructor_combination': team_constructors, 'expected_score': team_escore, 'cost': team_cost})
+    print("Calculations complete")
+    print()
 
     # Filter teams based on cost cap and sort by expected score
-    team_df = team_df[team_df['cost'] <= COST_CAP].sort_values(by='expected_score', ascending=False)
+    team_df = team_df[team_df['cost'] <= COST_CAP].sort_values(by='expected_score', ascending=False)  
 
-    # Print top 20 teams 
-    print(team_df.head(20))      
+    # Save teams to csv
+    path = os.path.join(cwd, "team_combinations.csv")
+    print("Saving teams to {}".format(path))
+    team_df.to_csv(path, index=False)
     
 
 if __name__ == "__main__":
